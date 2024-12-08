0fc97d2cd8a0defd11204d7127e36d00e66c36de
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-12-08 16:24:00 +0100

    prevent scrollable UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="after:bg-opaque relative isolate flex h-full flex-col items-center justify-between bg-welcome bg-cover bg-center pb-20 pt-8 font-bold text-white after:absolute after:-z-10 after:h-full after:w-full">
+    <div className="after:bg-opaque fixed isolate flex h-full w-full flex-col items-center justify-between overflow-hidden bg-welcome bg-cover bg-center pb-20 pt-8 font-bold text-white after:absolute after:-z-10 after:h-full after:w-full">
       <div className="flex flex-col items-center text-center">
         <Image
           alt="Ithaca"
