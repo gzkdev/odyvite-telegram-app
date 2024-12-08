acf86537e7e85227314f49fbf8f97a3f630a955b
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-12-08 15:43:02 +0100

    build home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,28 @@
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div className="text-4xl font-bold">Odyvite App</div>;
+  return (
+    <div className="after:bg-opaque relative isolate flex min-h-screen flex-col items-center justify-between bg-welcome bg-cover bg-center pb-20 pt-8 font-bold text-white after:absolute after:-z-10 after:h-full after:w-full">
+      <div className="flex flex-col items-center text-center">
+        <Image
+          alt="Ithaca"
+          src="/images/ithaca-logo.png"
+          width={196}
+          height={64}
+          className="translate-x-4"
+        />
+        <div className="text-shadow -translate-y-6 font-greek-freak text-3xl uppercase">
+          Presents
+        </div>
+        <div className="text-shadow mt-12 max-w-96 font-greek-freak text-8xl">
+          Odyssey of Fate
+        </div>
+      </div>
+
+      <Button variant="fantasy" className="p-3 text-3xl uppercase">
+        Begin Trial
+      </Button>
+    </div>
+  );
 }
