6033d2c139f00570225eb59a1562d0a574192347
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-12-08 09:11:30 +0100

    add font optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${augusta.variable} ${gemfontOne.variable} ${greekFreak.variable} ${greekRegular.variable} ${xtra.variable} font-sans`}
+        className={`${augusta.variable} ${gemfontOne.variable} ${greekFreak.variable} ${greekRegular.variable} ${xtra.variable} font-sans antialiased`}
       >
         {children}
       </body>
