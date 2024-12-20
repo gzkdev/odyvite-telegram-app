cdc460e428096b3ca6fa33d77e2b48096e423446
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-12-20 10:57:38 +0100

    refactor app ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,40 @@
-import Image from "next/image";
-import Link from "next/link";
+// import Link from "next/link";
+// import Image from "next/image";
+
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div className="after:bg-opaque fixed isolate flex h-full w-full flex-col items-center justify-between overflow-hidden bg-welcome bg-cover bg-center pb-20 pt-8 font-bold text-white after:absolute after:-z-10 after:h-full after:w-full">
-      <div className="flex flex-col items-center text-center">
-        <Image
-          alt="Ithaca"
-          src="/images/ithaca-logo.png"
-          width={196}
-          height={64}
-          className="translate-x-4"
-        />
-        <div className="text-shadow -translate-y-6 font-greek-freak text-3xl uppercase">
-          Presents
+    <>
+      <div className="flex h-full w-full flex-col items-center justify-between pb-20 pt-8 text-center">
+        {/* <div className="flex flex-col items-center text-center">
+          <Image
+            alt="Ithaca"
+            src="/images/ithaca-logo.png"
+            width={196}
+            height={64}
+            className="pointer-events-none translate-x-4"
+          />
+          <div className="text-shadow -translate-y-6 font-greek-freak text-3xl uppercase">
+            Presents
+          </div>
+          <div className="text-shadow mt-12 max-w-96 font-greek-freak text-8xl">
+            Odyssey of Fate
+          </div>
         </div>
-        <div className="text-shadow mt-12 max-w-96 font-greek-freak text-8xl">
-          Odyssey of Fate
-        </div>
-      </div>
 
-      <Link href="/" className="fantasy p-3 text-3xl uppercase">
-        Begin Trial
-      </Link>
-    </div>
+        <Link href="/prologue" className="fantasy p-3 text-3xl uppercase">
+          Begin Trial
+        </Link> */}
+        <div>Ithaca Odyssey App</div>
+
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+          iste qui. Id, quae laudantium? Aliquam vel ipsa esse nesciunt in!
+        </div>
+
+        <Button className="px-10 py-4 outline">Start Game </Button>
+      </div>
+    </>
   );
 }
