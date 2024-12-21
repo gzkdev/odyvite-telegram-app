e520c42dacb512befd1f131ec4e79db17b5048a1
Ezihe Godswill <ezihegodswill01@gmail.com>
2024-12-21 06:06:15 +0100

    refactor welcome screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,26 +3,28 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="after:bg-opaque fixed isolate flex h-full w-full flex-col items-center justify-between overflow-hidden bg-welcome bg-cover bg-center pb-20 pt-8 font-bold text-white after:absolute after:-z-10 after:h-full after:w-full">
-      <div className="flex flex-col items-center text-center">
-        <Image
-          alt="Ithaca"
-          src="/images/ithaca-logo.png"
-          width={196}
-          height={64}
-          className="translate-x-4"
-        />
-        <div className="text-shadow -translate-y-6 font-greek-freak text-3xl uppercase">
-          Presents
-        </div>
-        <div className="text-shadow mt-12 max-w-96 font-greek-freak text-8xl">
-          Odyssey of Fate
-        </div>
-      </div>
+    <div className="after:bg-curtain relative left-0 isolate h-full bg-welcome bg-cover bg-center after:absolute after:top-0 after:-z-10 after:h-full after:w-full">
+      <div className="max-w-screen-xs relative mx-auto flex h-full flex-col items-center justify-between px-6 pb-24 pt-8 font-greek-freak">
+        <div className="flex flex-col items-center text-center">
+          <Image
+            alt="Ithaca"
+            src="/images/ithaca-logo.png"
+            width={196}
+            height={64}
+            className="translate-x-4 translate-y-5"
+          />
 
-      <Link href="/" className="fantasy p-3 text-3xl uppercase">
-        Begin Trial
-      </Link>
+          <div className="text-shadow text-3xl uppercase">Presents</div>
+
+          <div className="text-shadow mt-12 text-balance text-8xl">
+            Odyssey of Fate
+          </div>
+        </div>
+
+        <Link href="/" className="fantasy-btn p-3 text-3xl uppercase">
+          Begin Trial
+        </Link>
+      </div>
     </div>
   );
 }
